5d108b88bf45c28e68dd2425e51b13e3f5062827
cfRIKO <40830087+rcfRIKO@users.noreply.github.com>
2023-03-26 21:30:51 +0200

    Added Featuresc

---
+++ src/components/consoleOutput/ConsoleOutput.tsx
@@ -0,0 +1,47 @@
+import ConsoleLink from "@components/consoleLink/ConsoleLink"
+import ConsolePrompt from "@components/consolePrompt/ConsolePrompt"
+import DisplayElement from "@interfaces/DisplayElement"
+import styles from "./ConsoleOutput.module.css"
+
+export default function ConsoleOutput({ data }: { data: DisplayElement }) {
+  const content: any[] = JSON.parse(data.value)
+  console.log(content)
+  let style
+  switch (content[0].style) {
+    case "folder":
+      style = styles.folderSpan
+      break;
+    case "error":
+      style = styles.errorSpan
+      break
+    case "file":
+      style = styles.fileSpan
+      break
+    default:
+      style = styles.normalSpan
+      break;
+  }
+
+  return <div className={styles.elementDiv}>
+    {
+      data.prompt ?
+        <ConsolePrompt prompt={data.prompt} />
+        : <span className={`${style} ${styles.elementSpan}`}>
+          {
+            content[0].url ?
+              <ConsoleLink url={content[0].url} text={content[0].value} />
+              : content[0].value
+          }
+        </span>
+    }
+    {
+      content.slice(1).map((element, idx) => <span key={idx} className={styles.elementSpan}>
+        {
+          content[idx + 1].url ?
+            <ConsoleLink url={content[idx + 1].url} text={content[idx + 1].value} />
+            : content[idx + 1].value
+        }
+      </span>)
+    }
+  </div>
+}
